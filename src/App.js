c34d2bfc77065a7c6e7db32ef1c49f8974353c5c
Irene <irene.nikkarinen@helsinki.fi>
2017-11-15 19:23:28 +0200

    Create react application base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello Remember me!
+      </div>
+    );
+  }
+}
+
+export default App;
