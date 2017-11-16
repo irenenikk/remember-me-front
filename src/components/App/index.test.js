e726c00e21891b28d4ea0686ccae89aa15444b4a
Irene <irene.nikkarinen@helsinki.fi>
2017-11-16 10:54:52 +0200

    Fix test

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './index';
+import { Provider } from 'react-redux';
+import createStore from '../../state/store';
 
 it('renders without crashing', () => {
+  const store = createStore();
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, div);
 });
 
