11c77b3985563b745ac45f38ae3bd33755b6c924
TainaLepisto <lepisto.taina@gmail.com>
2017-11-18 16:02:12 +0200

    the component library added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { clickAction } from '../../state/actions';
-import Form from '../Form';
+import FormAddNewBook from '../FormAddNewBook';
 import './index.css';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import {Tabs, Tab} from 'material-ui/Tabs';
+import FontIcon from 'material-ui/FontIcon';
+import AppBar from 'material-ui/AppBar';
+
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+// More on Colors: http://www.material-ui.com/#/customization/col
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#ff6f00',
+  },
+});
+
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1> Remember me! - Lukulista </h1>
-        <div>
+      <MuiThemeProvider className="App" muiTheme={muiTheme}>
 
-          <Form />
+        <AppBar
+          title="Remember me! - Lukulista"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
 
-        </div>
-      </div>
+        <Tabs>
+           <Tab
+             icon={<FontIcon className="material-icons">home</FontIcon>}
+             label="Lisää kirja">
+                <FormAddNewBook />
+          </Tab>
+           <Tab
+             icon={<FontIcon className="material-icons">favorite</FontIcon>}
+             label="Suosikit"
+           />
+
+         </Tabs>
+
+      </MuiThemeProvider>
     );
   }
 }
