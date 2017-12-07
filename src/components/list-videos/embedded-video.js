996f751f528b7324909e16b628a7aaf14f91b072
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2017-12-07 17:35:02 +0200

    add embedded video

---
+++ src/components/list-videos/embedded-video.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Youtube from 'react-youtube';
+import getYoutubeId from 'get-youtube-id';
+import { CardText } from 'material-ui/Card';
+
+export default ({ url }) => {
+    const regex = new RegExp("^https?://(www.)?youtube.");
+    if (regex.test(url)) {
+        const id = getYoutubeId(url);
+        return (
+            <CardText>
+                <Youtube
+                    videoId={id}
+                />
+            </CardText>
+        );
+    }
+    return (
+        <CardText>
+            <a href={url}>{url}</a>
+        </CardText>
+    );
+}
