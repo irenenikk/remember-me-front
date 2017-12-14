f0b5bb3793eff64c6b79d8d3210c26c3deda8dbc
Maaret Ihamäki <maaret.ihamaki@gmail.com>
2017-12-14 13:36:36 +0200

    fix validation tests

---
+++ features/done.feature
@@ -0,0 +1,8 @@
+# @done
+# Feature: see if tip is done Test
+
+# Scenario: I should be able to see tip marked as undone after submitting a new tip
+#         Given I navigate to the home page
+#         When I load the page
+#         And a book with title Harry Potter and author J.K. Rowling is submitted
+#         Then the book with title Harry Potter and author J.K. Rowling is marked undone
--- features/step_definitions/validation_steps.js
+++ features/step_definitions/validation_steps.js
@@ -3,20 +3,34 @@ const { expect } = require('code');
 
 defineSupportCode(function ({ Given, When, Then }) {
 
-    When ('a book with an empty title and author is submitted', function () {
+    When('a book with an empty title is submitted', function () {
         this.app.clickAddBookButton();
-        this.app.writeToBookAuthorInput("");
+        this.app.writeToBookAuthorInput("Author");
         this.app.writeToBookTitleInput("");
-        this.app.submitBookFormWitErrorValues();
+        this.app.submitBookFormWithErrorValues();
     });
 
-    Then('an error message with {string} as content and {string} as content is shown on the infobar', function (errorMessage1, errorMessage2) {
+    When('a book with an empty author is submitted', function () {
+        this.app.clickAddBookButton();
+        this.app.writeToBookAuthorInput("");
+        this.app.writeToBookTitleInput("Title");
+        this.app.submitBookFormWithErrorValues();
+    });
+
+    When('a blogpost with an invalid url is submitted', function () {
+        this.app.clickAddBlogpostButton();
+        this.app.writeToBlogpostAuthorInput("Great blog");
+        this.app.writeToBlogpostTitleInput("Great author");
+        this.app.writeToBlogpostLinkInput("youtube");
+        this.app.submitBlogpostFormWithErrorValues();
+    });
+
+    Then('an error message with {string} as content is shown on the infobar', function (errorMessage) {
         return this.app.getInfoBar()
             .end()
             .then((result) => {
-                expect(result.content).to.contain(errorMessage1);
-                expect(result.content).to.contain(errorMessage2);
+                expect(result.content).to.contain(errorMessage);
             });
-    });
+    })
 
 });
